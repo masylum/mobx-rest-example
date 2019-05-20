362c867c9bb14a2bc3b58a74e8eee078087b6f49
Gerard Clos <clos.gerard@gmail.com>
2019-05-20 14:53:20 +0200

    use computed in users collection example

--- src/stores/users.js
+++ src/stores/users.js
@@ -1,32 +1,20 @@
 import { Collection, Model } from 'mobx-rest'
-import { extendObservable } from 'mobx'
+import { computed } from 'mobx'
 
 class UserModel extends Model {
-  constructor (data) {
-    super(data)
-
-    // Example of a computed attribute
-    // If you have support for decorators that would
-    // just be:
-    //
-    // @computed get color () {
-    //   ...
-    // }
-    extendObservable(this, {
-      get color () {
-        const str = this.get('name')
-        let hash = 0
-        for (let i = 0; i < str.length; i++) {
-          hash = str.charCodeAt(i) + ((hash << 5) - hash)
-        }
-        let colour = '#'
-        for (let j = 0; j < 3; j++) {
-          const value = (hash >> (j * 8)) & 0xFF
-          colour += ('00' + value.toString(16)).substr(-2)
-        }
-        return colour
-      }
-    })
+  @computed
+  get color () {
+    const str = this.get('name')
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash)
+    }
+    let colour = '#'
+    for (let j = 0; j < 3; j++) {
+      const value = (hash >> (j * 8)) & 0xFF
+      colour += ('00' + value.toString(16)).substr(-2)
+    }
+    return colour
   }
 
   get initials () {
